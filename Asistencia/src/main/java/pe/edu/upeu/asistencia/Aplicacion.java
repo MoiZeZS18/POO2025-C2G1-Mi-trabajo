7b430146b567c3b739fb1edd6467e0e03489aa79
MoiZeZS18 <moises_17_hp@hotmail.com>
2025-08-24 22:42:53 -0500

    Subiendo mi Proyexto

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("Iniciando Aplicacion MZC");
+        AsistenciaApplication.main(args);
+    }
+}
